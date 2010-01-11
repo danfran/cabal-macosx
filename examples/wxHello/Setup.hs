387f47bfb824b777ed7d7ca353813dea4b922f21
Andy Gimblett <a.m.gimblett@swansea.ac.uk>
2010-01-11 15:36:46 +0000

    Removed vestigal example

--- examples/wxHello/Setup.hs
+++ examples/wxHello/Setup.hs
@@ -1,3 +1,5 @@
+-- Example Setup.hs for the wxHello app.
+
 import Distribution.MacOSX
 import Distribution.Simple
 import System.Info (os)
@@ -6,11 +8,22 @@ main :: IO ()
 main = defaultMainWithHooks $ addMacHook simpleUserHooks
   where addMacHook h =
           case os of
-            -- is it OK to treat darwin as synonymous with MacOS X?
+            -- Here darwin == Mac OS X.  Is that OK?
             "darwin" -> h { postBuild = appBundleBuildHook guiApps }
             _        -> h
 
+-- | appBundleHook's first argument is a Maybe [MacApp] containing
+-- names of executables (as found in your .cabal file) for which an
+-- app bundle should be built, and their and custom resources, if any.
+-- If it's Nothing, then an app bundle will be built for _all_ your
+-- executables (so in most simple cases, this is just what you want).
+-- (And of course Just [] means don't create app bundles for any of
+-- them.)
+--
+-- In future, MacApp values will probably carry other customisation
+-- information, such as paths of shared libraries to be baked into the
+-- bundle.
 guiApps :: Maybe [MacApp]
 guiApps = Just [("WxHello", [MacInfoPlist "resources/WxHello.plist", 
-                               MacIcon "resources/WxHello.icns"])
+                             MacIcon "resources/WxHello.icns"])
                ]
