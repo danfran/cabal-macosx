389078d0b607c8e6b9572fd874f03b8847ff9569
Andy Gimblett <a.m.gimblett@swansea.ac.uk>
2010-02-04 16:42:46 +0000

    Updated wxHello example to use new system

--- examples/wxHello/Setup.hs
+++ examples/wxHello/Setup.hs
@@ -2,28 +2,27 @@
 
 import Distribution.MacOSX
 import Distribution.Simple
-import System.Info (os)
+-- import System.Info (os)
 
 main :: IO ()
-main = defaultMainWithHooks $ addMacHook simpleUserHooks
-  where addMacHook h =
-          case os of
-            -- Here darwin == Mac OS X.  Is that OK?
-            "darwin" -> h { postBuild = appBundleBuildHook guiApps }
-            _        -> h
+main = defaultMainWithHooks $ simpleUserHooks {
+         postBuild = appBundleBuildHook guiApps
+       }
 
--- | appBundleHook's first argument is a Maybe [MacApp] containing
--- names of executables (as found in your .cabal file) for which an
--- app bundle should be built, and their and custom resources, if any.
--- If it's Nothing, then an app bundle will be built for _all_ your
--- executables (so in most simple cases, this is just what you want).
--- (And of course Just [] means don't create app bundles for any of
--- them.)
---
--- In future, MacApp values will probably carry other customisation
--- information, such as paths of shared libraries to be baked into the
--- bundle.
-guiApps :: Maybe [MacApp]
-guiApps = Just [("WxHello", [MacInfoPlist "resources/WxHello.plist", 
-                             MacIcon "resources/WxHello.icns"])
-               ]
+-- Checking by hand for OSX in Setup.hs no longer necessary.
+
+-- main = defaultMainWithHooks $ addMacHook simpleUserHooks
+--   where addMacHook h =
+--           case os of
+--             -- Here darwin == Mac OS X.  Is that OK?
+--             "darwin" -> h { postBuild = appBundleBuildHook guiApps }
+--             _        -> h
+
+guiApps :: [MacApp]
+guiApps = [MacApp "WxHello"
+                  (Just "resources/WxHello.icns")
+                  (Just "resources/WxHello.plist")
+                  [] -- No other resources.
+                  [] -- No other binaries.
+                  DoNotChase -- Try changing to ChaseWithDefaults
+          ]
