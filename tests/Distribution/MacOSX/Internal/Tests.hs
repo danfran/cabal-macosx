e5fe07642fc7011ed4e182206d1d2eca62811e53
NanoTech <nanotech@nanotechcorp.net>
2017-02-11 15:39:18 -0600

    Remove test for the removed Carbon tools step

--- tests/Distribution/MacOSX/Internal/Tests.hs
+++ tests/Distribution/MacOSX/Internal/Tests.hs
@@ -1,21 +1,18 @@
 module Distribution.MacOSX.Internal.Tests where
 
-import System.IO.Temp (withSystemTempDirectory)
-import Control.Exception (SomeException, catch)
 import Prelude hiding (catch)
 import Test.HUnit (Assertion, assertEqual)
 import Test.Framework (Test, mutuallyExclusive, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Distribution.PackageDescription (BuildInfo(..), Executable(..), emptyBuildInfo)
 
-import Distribution.MacOSX.Internal (osxIncantations, getMacAppsForBuildableExecutors)
+import Distribution.MacOSX.Internal (getMacAppsForBuildableExecutors)
 import Distribution.MacOSX.Common
 
 macosxInternalTests :: Test
 macosxInternalTests = testGroup "Distribution.MacOSX.Internal"
     [ mutuallyExclusive $ testGroup "MacOSX Internal"
-        [ testCase "should exit with an exit-failure as Xcode's Carbon Tools fail to run" testCarbonTools,
-          -- I should consider to use QuickCheck maybe... :)
+        [ -- I should consider to use QuickCheck maybe... :)
           testCase "given nothing then should not try to build any mac-app" testBuildMacApp_noInput,
           testCase "given no executables then should not try to build any mac-app" testBuildMacApp_noExecutables,
           testCase "given only two executables then should try to build two mac-apps" testBuildMacApp_twoBuildableExecutables,
@@ -23,15 +20,6 @@ macosxInternalTests = testGroup "Distribution.MacOSX.Internal"
           testCase "given two executables and one not executable and two apps then should try to build one mac-app" testBuildMacApp_twoAppsAndTwoExecutablesOneBuildableOneNot
         ]
     ]
-
-testCarbonTools :: Assertion
-testCarbonTools = do
-    let macApp = MacApp "DummyApp" Nothing Nothing [] [] DoNotChase
-
-    withSystemTempDirectory "DummyAppPath" $ \tmpDir
-        -> osxIncantations tmpDir macApp -- some problems here to add `assertFailure` after
-             `catch`
-                 (\e -> putStrLn $ "Catched: " ++ show (e :: SomeException))
 
 testBuildMacApp_noInput :: Assertion
 testBuildMacApp_noInput = do
